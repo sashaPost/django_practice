c25fabbd360024ee5493a304439b3bc53637d13a
SashaPost <postilha.a@gmail.com>
2022-11-14 18:51:49 +0200

    Wrote a function to render CV (pdf)

---
+++ cv_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import FileResponse
+import os
+
+# Create your views here.
+def show_pdf(request):
+    filepath = os.path.join('media', 'postylha.o_cv.pdf')
+    return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
